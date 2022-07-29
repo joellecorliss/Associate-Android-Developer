683e89acca96551172f02e1ecb297cbdb6098841
joellecorliss <26968011+joellecorliss@users.noreply.github.com>
2022-07-30 00:49:43 +0800

    Bus Schedule App

---
+++ Unit-5_Data-persistence/android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -0,0 +1,17 @@
+package com.example.busschedule.database.schedule
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+// The DAO is where you would include functions for reading and manipulating data.
+// Calling a function on the DAO is the equivalent of performing a SQL command on the database.
+
+@Dao //to make the interface usable with Room.
+interface ScheduleDao {
+    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    fun getAll(): Flow<List<Schedule>>
+
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
+    fun getByStopName(stopName: String): Flow<List<Schedule>>
+}
